425ce4d4eb97ddeaa9800d8772c12e6fa82dab1c
pdxiv <pdxiv@github.com>
2021-05-27 22:49:00 +0200

    put colored things into main code

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"image/color"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -41,6 +45,19 @@ var treeUIDMapping = map[string][]string{
 	"subnode_5_XXX": {"subnode_5_ZZZ"},
 }
 
+var data = []string{
+	"2021-09-22 09:23:12 INFO    Something happened, but it looks okay to me",
+	"2021-09-22 09:53:32 WARNING Something mighty strange happpened",
+	"2021-09-22 10:23:41 ERROR   Gosh dang! This is bad!",
+	"2021-09-22 11:15:12 INFO    Nevermind, it's fine!",
+}
+
+var severityColor = []color.RGBA{
+	{G: 127, A: 100},
+	{R: 255, G: 127, A: 100},
+	{R: 255, A: 100},
+}
+
 func main() {
 	myApp := app.New()
 	myWindow := myApp.NewWindow("TabContainer Widget")
@@ -54,10 +71,15 @@ func main() {
 func makeSplittyBoiWithTreeAndTable() *container.Split {
 	left := makeTree()
 
-	tableThing := makeTable()
+	labelThing := widget.NewLabel("Top right side of splitter")
+	labelThing.TextStyle.Monospace = true
+
+	tableThing := makeColorTable()
+	// tableThing := makeTable()
+	// tableThing := makeList()
 
 	right := container.NewVSplit(
-		widget.NewLabel("Top right side of splitter"),
+		labelThing,
 		tableThing,
 	)
 
@@ -65,13 +87,64 @@ func makeSplittyBoiWithTreeAndTable() *container.Split {
 	return horizontalSplitter
 }
 
+func makeList() *widget.List {
+	listThing := widget.NewList(
+		func() int {
+			linesInList := len(data)
+			return linesInList
+		},
+		func() fyne.CanvasObject {
+			return container.New(layout.NewMaxLayout(),
+				canvas.NewRectangle(color.RGBA{G: 155, A: 100}),
+				widget.NewLabel("Item x"))
+		},
+
+		func(i widget.ListItemID, o fyne.CanvasObject) {
+			box := o.(*fyne.Container)
+			rect := box.Objects[0].(*canvas.Rectangle)
+			rect.FillColor = severityColor[i%len(severityColor)]
+			label := box.Objects[1].(*widget.Label)
+			label.TextStyle.Monospace = true
+			label.SetText(data[i])
+		},
+	)
+	return listThing
+}
+
+func makeColorTable() *widget.Table {
+	tableThing := widget.NewTable(
+		func() (int, int) {
+			rows := len(tableData)
+			columns := len(tableData[0])
+			return rows, columns
+		},
+		func() fyne.CanvasObject {
+			return container.New(layout.NewMaxLayout(),
+				canvas.NewRectangle(color.RGBA{G: 155, A: 100}),
+				widget.NewLabel("wide contentXXXXX"))
+		},
+		func(i widget.TableCellID, o fyne.CanvasObject) {
+			box := o.(*fyne.Container)
+			rect := box.Objects[0].(*canvas.Rectangle)
+			rect.FillColor = severityColor[i.Row%len(severityColor)]
+			label := box.Objects[1].(*widget.Label)
+			label.SetText(tableData[i.Row][i.Col])
+		})
+	return tableThing
+}
+
 func makeTable() *widget.Table {
 	tableThing := widget.NewTable(
 		func() (int, int) {
-			return len(tableData), len(tableData[0])
+			rows := len(tableData)
+			columns := len(tableData[0])
+			return rows, columns
 		},
 		func() fyne.CanvasObject {
-			return widget.NewLabel("wide content")
+			labelToUse := widget.NewLabel("wide contentXXXXX")
+			// labelToUse.TextStyle.Monospace = true
+
+			return labelToUse
 		},
 		func(i widget.TableCellID, o fyne.CanvasObject) {
 			o.(*widget.Label).SetText(tableData[i.Row][i.Col])
